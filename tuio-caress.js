961479ab48e3bb78a0223b793921b8e1a6257e8f
Mark C Mitchell <mark@markcmitchell.net>
2015-04-21 22:49:33 +1000

    Remove client testing code.

--- tuio-caress.js
+++ tuio-caress.js
@@ -5,38 +5,6 @@ if(Meteor.isClient) {
             port: 5000
         });
         client.connect();
-
-        document.addEventListener('touchmove', onTouchMove, false);
-        document.addEventListener('touchstart', onTouchStart, false);
-
-        var debugs = [];
-
-        function onTouchStart(e)  {
-            $debug = $('<div class="debug"></div>');
-            $debug.css({
-                'position': 'absolute',
-                'left': e.clientX,
-                'top': e.clientY,
-                'background-color': 'red',
-                'width': '40px',
-                'height': '40px'
-            });
-            console.log(e);
-            $('body').append($debug);
-            debugs.push($debug);
-            //mouse.set(((e.clientX / window.innerWidth)*2) - 1, -((e.clientY/window.innerHeight) * 2) + 1, 1);
-            //debugMouse.set(e.clientX, e.clientY, 1);
-        }
-        function onTouchMove(e) {
-            for(var i=0;i<debugs.length;i++) {
-                $debug = debugs[i];
-                $debug.css({
-                    'left': e.clientX,
-                    'top': e.clientY
-                })
-            }
-        }
-
     });
 
 }
