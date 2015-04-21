b633726f69be174068b02497c9e390cbd8a7fe1a
Mark C Mitchell <mark@markcmitchell.net>
2015-04-21 23:43:40 +1000

    Add a unit test that checks whether Caress.Client connects properly to the socket.io websocket server

--- tuio-caress-tests.js
+++ tuio-caress-tests.js
@@ -1,5 +1,16 @@
-// Write your tests here!
-// Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
-});
+if(Meteor.isClient) {
+  Tinytest.add('my own example', function(test) {
+    window.client = new Caress.Client({
+      host: 'localhost',
+      port: 5000
+    });
+    client.connect();
+
+    // wait a moment for client to connect (client.connect() has no callback ability)
+    setTimeout(function(){
+      test.isTrue(client.connected, "client should have connected to websocket server served by socket.io");
+    }, 1000);
+  });
+}
+
+
