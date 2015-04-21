67d28a7402720ddae1b1ce2e32157f110db3bc8f
Mark C Mitchell <mark@markcmitchell.net>
2015-04-21 22:13:53 +1000

    Add git repo URL

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Converts TUIO events into W3C Touch Events. This is a meteor wrapper for the caress server/client combo by ekryski.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/zlot/meteor-tuio-caress',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
