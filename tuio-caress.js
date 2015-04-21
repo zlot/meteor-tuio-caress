ef302012e035af732e794e06e4e88ef8fd70648d
Mark C Mitchell <mark@markcmitchell.net>
2015-04-21 22:46:21 +1000

    Turn off debug mode for CaressServer.

--- tuio-caress.js
+++ tuio-caress.js
@@ -48,7 +48,7 @@ if(Meteor.isServer) {
         var io = Meteor.npmRequire('socket.io')(5000);
 
         var CaressServer = Meteor.npmRequire('caress-server');
-        var caressServer = new CaressServer('0.0.0.0', 3333, {debug: true, json: true});
+        var caressServer = new CaressServer('0.0.0.0', 3333, {debug: false, json: true});
 
         io.on('connection', function(socket) {
             caressServer.on('tuio', function(msg) {
