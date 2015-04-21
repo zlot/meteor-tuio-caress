9a172678b804850d28a6d93fac64fb13e2c6493c
Mark C Mitchell <mark@markcmitchell.net>
2015-04-21 23:00:06 +1000

    Update version to 0.1.0, for publishing

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zlot:tuio-caress',
-  version: '0.0.1',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Converts TUIO events into W3C Touch Events. This is a meteor wrapper for the caress server/client combo by ekryski.',
   // URL to the Git repository containing the source code for this package.
